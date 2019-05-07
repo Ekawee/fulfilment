0def590115925961e0761f96b57f1af408745caf
Ekawee Poolchu <ekawee.po@easysunday.com>
2019-05-07 21:19:08 +0700

    include price and dayCount

--- warehouse/__tests__/service/inventory.test.js
+++ warehouse/__tests__/service/inventory.test.js
@@ -1,10 +1,17 @@
 import { find } from 'lodash/fp';
 import serviceInventory from '../../src/service/inventory';
 import model from '../../src/model';
+import service from '../../src/service';
 
 jest.mock('../../src/model', () => ({
   inventory: {
     findAll: jest.fn(),
+  },
+}));
+
+jest.mock('../../src/service', () => ({
+  pricing: {
+    calculateInventoryDeposited: jest.fn(),
   },
 }));
 
@@ -13,39 +20,6 @@ describe('/service/inventory/getByDepositeReceiptPk', () => {
     jest.clearAllMocks();
   });
 
-  const mockInventoriesResponse = [
-    {
-      id: '1',
-      inventoryTypeId: '1',
-      depositReceiptId: 'deposit-receipt-id-1',
-      dispatchReceiptId: '1',
-      width: '5',
-      height: '3',
-      length: '10',
-      weight: null,
-      depositedAt: '2019-05-02T14:30:20.000Z',
-      dispatchedAt: '2019-05-04T20:13:05.000Z',
-      expectedAmount: '300',
-      paidAmount: '300',
-      status: 'PAID',
-    },
-    {
-      id: '2',
-      inventoryTypeId: '1',
-      depositReceiptId: 'deposit-receipt-id-2',
-      dispatchReceiptId: '1',
-      width: '5',
-      height: '3',
-      length: '10',
-      weight: null,
-      depositedAt: '2019-05-02T14:30:20.000Z',
-      dispatchedAt: '2019-05-04T20:13:05.000Z',
-      expectedAmount: '300',
-      paidAmount: '300',
-      status: 'PAID',
-    },
-  ];
-
   it('should return invetories list by using depositReceiptId (found)', async () => {
     const mockDepositReceiptId = 'deposit-receipt-id-1';
 
@@ -53,9 +27,60 @@ describe('/service/inventory/getByDepositeReceiptPk', () => {
       transaction: 'transaction-id-12345',
     };
 
+    const mockInventoriesResponse = [
+      {
+        id: 'inventory-id-1',
+        inventoryTypeId: '1',
+        depositReceiptId: 'deposit-receipt-id-1',
+        dispatchReceiptId: '1',
+        width: '5',
+        height: '3',
+        length: '10',
+        weight: null,
+        depositedAt: '2019-05-02T14:30:20.000Z',
+        dispatchedAt: '2019-05-04T20:13:05.000Z',
+        expectedAmount: '300',
+        paidAmount: '300',
+        status: 'PAID',
+        price: '1000',
+        dayCount: '10',
+      },
+      {
+        id: 'inventory-id-2',
+        inventoryTypeId: '1',
+        depositReceiptId: 'deposit-receipt-id-2',
+        dispatchReceiptId: '1',
+        width: '5',
+        height: '3',
+        length: '10',
+        weight: null,
+        depositedAt: '2019-05-02T14:30:20.000Z',
+        dispatchedAt: '2019-05-04T20:13:05.000Z',
+        expectedAmount: '300',
+        paidAmount: '300',
+        status: 'PAID',
+        price: '2000',
+        dayCount: '9',
+      },
+    ];
+
+    const pricingResponse = [
+      {
+        id: 'inventory-id-1',
+        price: '1000',
+        dayCount: '10',
+      },
+      {
+        id: 'inventory-id-2',
+        price: '2000',
+        dayCount: '9',
+      },
+    ]
+
     model.inventory.findAll = jest.fn(
       ({ where: { depositReceiptId } }) => [find({ depositReceiptId })(mockInventoriesResponse)]
       );
+    service.pricing.calculateInventoryDeposited = jest.fn((id) => find({ id })(pricingResponse));
 
     const result = await serviceInventory.getByDepositeReceiptPk(mockDepositReceiptId, mockModelOptions);
 
